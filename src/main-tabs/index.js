23756544043d1c07e961b68e022a38e0f0535cfe
Kagami Hiiragi <kagami@genshiken.org>
2016-11-23 19:11:03 +0300

    Main tabs handling

--- src/main-tabs/index.js
+++ src/main-tabs/index.js
@@ -4,6 +4,8 @@
  */
 
 import React from "react";
+import cx from "classnames";
+import Icon from "react-fa";
 import {useSheet} from "../jss";
 import {Tabs, Tab} from "../theme";
 import Instance from "./instance";
@@ -14,30 +16,52 @@ const KEY_9 = 57;
 
 @useSheet({
   tab: {
-    // XXX(Kagami): The only way to style tab header height.
-    // See: <https://github.com/callemall/material-ui/issues/5391>.
-    "& > div > div": {
+    flex: 1,
+    lineHeight: "40px",
+    color: "#fff !important",
+    backgroundColor: "#bbb !important",
+    // TODO(Kagami): The only way to style tab header height. See:
+    // <https://github.com/callemall/material-ui/issues/5391>.
+    // Report that.
+    "& > div": {
       height: "40px !important",
     },
   },
+  newTab: {
+    extend: "tab",
+    flex: "0 60px",
+  },
+  activeTab: {
+    color: "#999 !important",
+    backgroundColor: "#eee !important",
+  },
+  label: {
+    width: "100%",
+    padding: "0 5px",
+    boxSizing: "border-box",
+  },
+  icon: {
+    float: "right",
+    fontSize: "30px",
+    lineHeight: "40px",
+    marginTop: -1,
+  },
+  iconNew: {
+    fontSize: "30px",
+    width: 60,
+    lineHeight: "40px",
+  },
+  tabContent: {
+    position: "fixed",
+    left: 0,
+    right: 0,
+    top: 40,
+    bottom: 0,
+  },
 })
 export default class extends React.Component {
-  static styles = {
-    tabItem: {
-      backgroundColor: "#bbb",
-    },
-    inkBar: {
-      display: "none",
-    },
-    tabContent: {
-      position: "fixed",
-      left: 0,
-      right: 0,
-      top: 40,
-      bottom: 0,
-    },
-  }
   state = {tabs: [], tabIndex: 0}
+  tabKey = 0
   componentWillMount() {
     this.addTab();
   }
@@ -47,64 +71,107 @@ export default class extends React.Component {
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleGlobaKey, false);
   }
-  addTab() {
+  addTab(label = DEFAULT_LABEL) {
     const tabs = this.state.tabs;
-    tabs.push({label: DEFAULT_LABEL});
+    const key = this.tabKey++;
+    tabs.push({key, label});
     this.setState({tabs});
   }
-  handleChange = (tabIndex) => {
-    const tabs = this.state.tabs;
-    if (tabIndex === "new_tab") {
-      tabIndex = tabs.length;
-      this.addTab();
-      this.setState({tabIndex});
-    } else {
-      this.setState({tabIndex});
-    }
-  };
-  handleTabTitle = (i, label = DEFAULT_LABEL) => {
-    const tabs = this.state.tabs;
-    tabs[i].label = label;
-    this.setState({tabs});
-  };
   handleGlobaKey = (e) => {
     if (e.keyCode >= KEY_0 && e.keyCode <= KEY_9 && e.altKey) {
       let tabIndex = e.keyCode - KEY_0;
       tabIndex = (tabIndex === 0 ? 10 : tabIndex) - 1;
-      if (tabIndex < this.state.tabs.length &&
-          tabIndex !== this.state.tabIndex) {
+      if (tabIndex < this.state.tabs.length) {
         this.setState({tabIndex});
       }
     }
   };
+  handleTitleChange = (i, label = DEFAULT_LABEL) => {
+    const tabs = this.state.tabs;
+    tabs[i].label = label;
+    this.setState({tabs});
+  };
+  handleSelect = (tabIndex, e) => {
+    this.setState({tabIndex});
+  };
+  handleNew = (e) => {
+    e.stopPropagation();
+    this.addTab();
+    const tabIndex = this.state.tabs.length - 1;
+    this.setState({tabIndex});
+  };
+  handleClose = (i, e) => {
+    e.stopPropagation();
+    const tabs = this.state.tabs;
+    tabs.splice(i, 1);
+    let tabIndex = this.state.tabIndex;
+    tabIndex = Math.max(0, tabIndex);
+    tabIndex = Math.min(tabIndex, tabs.length - 1);
+    this.setState({tabs, tabIndex});
+  };
+  getLabelNode(label, i) {
+    const {classes} = this.sheet;
+    return (
+      <div className={classes.label}>
+        {label}
+        <Icon
+          name="remove"
+          className={classes.icon}
+          onTouchTap={this.handleClose.bind(null, i)}
+        />
+      </div>
+    );
+  }
+  getLabelNewNode() {
+    const {classes} = this.sheet;
+    return (
+      <Icon
+        name="plus"
+        title="New tab"
+        className={classes.iconNew}
+        onTouchTap={this.handleNew}
+      />
+    );
+  }
+  getTabTemplate({children, selected}) {
+    const style = {
+      display: selected ? "block" : "none",
+      height: "100%",
+    };
+    return <div style={style}>{children}</div>;
+  }
   render() {
     const {classes} = this.sheet;
-    const styles = this.constructor.styles;
     return (
       <Tabs
         value={this.state.tabIndex}
-        onChange={this.handleChange}
-        tabItemContainerStyle={styles.tabItem}
-        inkBarStyle={styles.inkBar}
-        tabTemplateStyle={styles.tabContent}
+        onChange={this.handleSelect}
+        inkBarStyle={{display: "none"}}
+        contentContainerClassName={classes.tabContent}
+        tabTemplate={this.getTabTemplate}
       >
       {this.state.tabs.map((tab, i) =>
         <Tab
-          key={i}
+          key={tab.key}
+          tabKey={tab.key}
           value={i}
-          label={tab.label}
-          className={classes.tab}
+          label={this.getLabelNode(tab.label, i)}
+          className={cx(classes.tab,
+                        this.state.tabIndex === i && classes.activeTab)}
+          disableTouchRipple
         >
           <Instance
             active={this.state.tabIndex === i}
-            onTabTitle={this.handleTabTitle.bind(null, i)}
+            onTabTitle={this.handleTitleChange.bind(null, i)}
           />
         </Tab>
       )}
         <Tab
-            label="new tab"
-            value="new_tab"
-            className={classes.tab}
+          tabKey={-1}
+          value={-1}
+          label={this.getLabelNewNode()}
+          className={classes.newTab}
+          disableTouchRipple
         />
       </Tabs>
     );
