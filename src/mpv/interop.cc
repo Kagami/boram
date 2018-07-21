57cac35cfe7a43881077408eba628539cde8daf0
Kagami Hiiragi <kagami@genshiken.org>
2018-07-21 16:54:18 +0300

    Remove vf-defaults setting
    Deprecated and no longer needed since
    https://github.com/mpv-player/mpv/commit/830f0aed97da7257e1cbe30a367144bc51ec52d2

--- src/mpv/interop.cc
+++ src/mpv/interop.cc
@@ -310,7 +310,6 @@ class BoramInstance : public pp::Instance {
     if (mpv_set_option_string(mpv_, "vo", "opengl-cb") < 0)
       DIE("failed to set VO");
 
-    mpv_set_option_string(mpv_, "vf-defaults", "yadif=interlaced-only=no");
     mpv_set_option_string(mpv_, "stop-playback-on-init-failure", "no");
     mpv_set_option_string(mpv_, "audio-file-auto", "no");
     mpv_set_option_string(mpv_, "sub-auto", "no");
